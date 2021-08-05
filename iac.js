f8aac09881daad5bfeaa59eeffb79038a3cc4545
alice <alice@mail.com>
2021-08-05 10:47:46 -0300

    new infra project

--- iac.js
+++ iac.js
@@ -2,22 +2,150 @@ const assert = require("assert");
 const { AzureProvider } = require("@grucloud/provider-azure");
 const hook = require("./hook");
 
-const createResources = async ({ provider, resources: {} }) => {
+const createResources = async ({ provider }) => {
   const { stage } = provider.config;
+  assert(stage);
+  // https://docs.microsoft.com/en-us/rest/api/apimanagement/2019-12-01/apimanagementservice/createorupdate
+  const rg = provider.resourceManagement.makeResourceGroup({
+    name: `resource-group-${stage}`,
+  });
 
-  return {};
+  // https://docs.microsoft.com/en-us/rest/api/virtualnetwork/virtualnetworks/createorupdate#request-body
+  const vnet = provider.virtualNetworks.makeVirtualNetwork({
+    name: `virtual-network-${stage}`,
+    dependencies: { resourceGroup: rg },
+    properties: () => ({
+      properties: {
+        addressSpace: { addressPrefixes: ["10.0.0.0/16"] },
+        subnets: [
+          {
+            name: `subnet-${stage}`,
+            properties: {
+              addressPrefix: "10.0.0.0/24",
+            },
+          },
+        ],
+      },
+    }),
+  });
+
+  // https://docs.microsoft.com/en-us/rest/api/virtualnetwork/networksecuritygroups/createorupdate#request-body
+  const sg = provider.virtualNetworks.makeSecurityGroup({
+    name: `security-group-${stage}`,
+    dependencies: { resourceGroup: rg },
+    properties: () => ({
+      properties: {
+        securityRules: [
+          {
+            name: "SSH",
+            properties: {
+              access: "Allow",
+              direction: "Inbound",
+              protocol: "Tcp",
+              destinationPortRange: "22",
+              destinationAddressPrefix: "*",
+              sourcePortRange: "*",
+              sourceAddressPrefix: "*",
+              priority: 1000,
+            },
+          },
+          {
+            name: "ICMP",
+            properties: {
+              access: "Allow",
+              direction: "Inbound",
+              protocol: "Icmp",
+              destinationAddressPrefix: "*",
+              destinationPortRange: "*",
+              sourceAddressPrefix: "*",
+              sourcePortRange: "*",
+              priority: 1001,
+            },
+          },
+        ],
+      },
+    }),
+  });
+
+  // https://docs.microsoft.com/en-us/rest/api/virtualnetwork/publicipaddresses/createorupdate#request-body
+  const publicIpAddress = provider.virtualNetworks.makePublicIpAddress({
+    name: `ip-${stage}`,
+    dependencies: {
+      resourceGroup: rg,
+    },
+    properties: () => ({
+      properties: {
+        publicIPAllocationMethod: "Dynamic",
+      },
+    }),
+  });
+  // https://docs.microsoft.com/en-us/rest/api/virtualnetwork/networkinterfaces/createorupdate#request-body
+  const networkInterface = provider.virtualNetworks.makeNetworkInterface({
+    name: `network-interface-${stage}`,
+    dependencies: {
+      resourceGroup: rg,
+      virtualNetwork: vnet,
+      securityGroup: sg,
+      subnet: `subnet-${stage}`,
+      publicIpAddress,
+    },
+    properties: () => ({
+      properties: {
+        ipConfigurations: [
+          {
+            name: "ipconfig",
+            properties: {
+              privateIPAllocationMethod: "Dynamic",
+            },
+          },
+        ],
+      },
+    }),
+  });
+
+  const { MACHINE_ADMIN_USERNAME, MACHINE_ADMIN_PASSWORD } = process.env;
+  assert(MACHINE_ADMIN_USERNAME);
+  assert(MACHINE_ADMIN_PASSWORD);
+
+  // https://docs.microsoft.com/en-us/rest/api/compute/virtualmachines/createorupdate
+  const vm = provider.compute.makeVirtualMachine({
+    name: `vm-${stage}`,
+    dependencies: {
+      resourceGroup: rg,
+      networkInterface: networkInterface,
+    },
+    properties: () => ({
+      properties: {
+        hardwareProfile: {
+          vmSize: "Standard_A1_v2",
+        },
+        storageProfile: {
+          imageReference: {
+            // az vm image list
+            offer: "UbuntuServer",
+            publisher: "Canonical",
+            sku: "18.04-LTS",
+            version: "latest",
+          },
+        },
+        osProfile: {
+          adminUsername: MACHINE_ADMIN_USERNAME,
+          computerName: "myVM",
+          adminPassword: MACHINE_ADMIN_PASSWORD,
+        },
+      },
+    }),
+  });
+  return { rg, sg, vnet, publicIpAddress, networkInterface, vm };
 };
+
 exports.createResources = createResources;
 
-exports.createStack = async () => {
-  const provider = AzureProvider({ config: require("./config") });
-  const { stage } = provider.config;
-  assert(stage, "missing stage");
-
-  const resources = await createResources({
-    provider,
-    resources: {},
+exports.createStack = async ({ createProvider }) => {
+  const provider = createProvider(AzureProvider, {
+    config: require("./config"),
   });
+  const resources = await createResources({ provider });
 
   return {
     provider,
